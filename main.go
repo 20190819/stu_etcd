7f89c5127ffbced06333af887303a61378e08ac1
杨亮 <yangliang@114study.com>
2021-10-09 18:24:14 +0800

    etcd 监听

--- main.go
+++ main.go
@@ -6,40 +6,68 @@ import (
 	"fmt"
 	"time"
 
-	c3 "github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/clientv3"
+	"github.com/coreos/etcd/mvcc/mvccpb"
 )
 
-var config c3.Config
-var client *c3.Client
+var config clientv3.Config
 
-const k1 = "/cron/jobs/job1"
+const pre = "/cron/jobs/"
 
 func main() {
 
-	config = c3.Config{
+	config = clientv3.Config{
 		Endpoints:   []string{"http://127.0.0.1:4379", "http://127.0.0.1:2379", "http://127.0.0.1:3379"},
 		DialTimeout: time.Second * 5,
 	}
 
-	client, err := c3.New(config)
+	client, err := clientv3.New(config)
 	exception.Handler(err)
 
-	kv := c3.NewKV(client)
-	resp, err := kv.Put(context.TODO(), k1, "bye", c3.WithPrevKV())
-	exception.Handler(err)
-
-	//获取版本信息
-	fmt.Println("Revision", resp.Header.Revision)
-	if resp.PrevKv != nil {
-		fmt.Println("key:", string(resp.PrevKv.Key))
-		fmt.Println("Value:", string(resp.PrevKv.Value))
-		fmt.Println("Version:", rune(resp.PrevKv.Version))
+	kv := clientv3.NewKV(client)
+	// kv.Delete(context.TODO(), pre, clientv3.WithPrefix())
+	for i := 0; i < 10; i++ {
+		_, err := kv.Put(context.TODO(), fmt.Sprintf("%s%d", pre, i), "bye"+time.Now().String(), clientv3.WithPrevKV())
+		exception.Handler(err)
 	}
+	fmt.Println("add success")
 
-	// 读取键值对
+	go func() {
+		for {
+			kv.Put(context.TODO(), "/cron/jobs/7", "localhost:8080")
+			kv.Delete(context.TODO(), "/cron/jobs/7")
+			time.Sleep(3 * time.Second)
+		}
+	}()
 
-	readResp, err := kv.Get(context.Background(), k1)
+	getResp, err := kv.Get(context.TODO(), "/cron/jobs/7")
 	exception.Handler(err)
-	fmt.Println("读取信息：", readResp.Kvs)
 
+	// 当前etcd集群事务ID, 单调递增的
+	watchStartRevision := getResp.Header.Revision + 1
+
+	// 创建一个watcher
+	watcher := clientv3.NewWatcher(client)
+
+	// 启动监听
+	fmt.Println("从该版本向后监听:", watchStartRevision)
+
+	ctx, cancelFunc := context.WithCancel(context.TODO())
+	time.AfterFunc(20*time.Second, func() {
+		cancelFunc()
+	})
+	ops := []clientv3.OpOption{clientv3.WithRev(watchStartRevision)}
+	watchRespChan := watcher.Watch(ctx, "/cron/jobs/7", ops...)
+
+	// 处理kv变化事件
+	for resp := range watchRespChan {
+		for _, ev := range resp.Events {
+			switch ev.Type {
+			case mvccpb.PUT:
+				fmt.Println("handler put")
+			case mvccpb.DELETE:
+				fmt.Println("handler delete")
+			}
+		}
+	}
 }
